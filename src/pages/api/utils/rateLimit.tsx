9783e6d4998bd8924ff3d2b2c08524336aecf5a4
Janak <lightlife908@gmail.com>
2025-06-02 22:24:26 +0545

    code changes

--- src/pages/api/utils/rateLimit.tsx
+++ src/pages/api/utils/rateLimit.tsx
@@ -1,7 +1,5 @@
-
-// import rateLimit from "express-rate-limit";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { RequestHandler } from "express";
+import type { RequestHandler, Request, Response } from "express";
 
 // Convert Express middleware to work with Next.js
 export function runMiddleware(
@@ -10,7 +8,7 @@ export function runMiddleware(
   fn: RequestHandler
 ): Promise<void> {
   return new Promise((resolve, reject) => {
-    fn(req as any, res as any, (result: unknown) => {
+    fn(req as unknown as Request, res as unknown as Response, (result: unknown) => {
       if (result instanceof Error) {
         return reject(result);
       }
